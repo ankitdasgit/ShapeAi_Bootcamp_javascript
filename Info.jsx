2bb7b7e701505a325a7f81a1fc3a4c14d55b5937
ankit kumar das <76767906+ankitdasgit@users.noreply.github.com>
2021-06-26 19:15:23 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+<h1>JavaScript and React.js</h1>
+<p>basic java script and react js bootcamp by ShapeAi</p>
+  </div>
+  );
+}
+
+export default Info;
